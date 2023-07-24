9176b3d32557b9ab2abd2ae6b21e6582fad10005
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-24 18:40:02 -0400

    Nuevo componente Nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script>
+  import { Nav } from './components/Nav.vue'
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      Nav
+    }
   }
 </script>
 
@@ -8,21 +13,7 @@
 
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-light fixed-top">
-      <div class="container">
-        <a href="#" class="navbar-brand">Home</a>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="#" class="nav-link">Login</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Sign-up</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Nav />
 
     <div class="auth-wrapper">
       <div class="auth-inner">
