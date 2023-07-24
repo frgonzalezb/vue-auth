25d1b4be4373a137de08173bf5a31c0d25cc9b19
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-24 19:06:12 -0400

    router.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { Router } from './router.js'
+
 createApp(App).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { Vue } from 'vue'
+import { Router } from 'vue-router'
+
+import { Home } from 'components/Home.vue'
+import { Login } from 'components/Login.vue'
+
+
+Vue.use(Router)
+
+export default Router ({
+  routes: [
+    {path: '/', component: Home},
+    {path: '/login', component: Login},
+  ]
+})
