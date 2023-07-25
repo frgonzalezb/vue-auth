375cde96aa78c611f5a37c9a490b2f27d15f18aa
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-25 17:09:11 -0400

    Update NavbarItem.vue

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar navbar-expand navbar-light fixed-top">
     <div class="container">
-      <a href="#" class="navbar-brand">Home</a>
+      <router-link to="/" class="navbar-brand">Home</router-link>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a href="#" class="nav-link">Login</a>
+            <router-link to="/login" class="nav-link">Login</router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">Sign-up</a>
+            <router-link to="/register" class="nav-link">Sign-up</router-link>
           </li>
         </ul>
       </div>
