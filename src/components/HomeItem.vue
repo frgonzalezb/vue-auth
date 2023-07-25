001ba3f6b086e1296fad72e23229ca09a14eccae
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-25 16:47:49 -0400

    Update HomeItem.vue

--- src/components/HomeItem.vue
+++ src/components/HomeItem.vue
@@ -10,7 +10,13 @@
   export default {
     name: 'HomeItem',
     async created() {
-      const response = await axios.get('user');
+      const headers = {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token')
+        }
+      }
+
+      const response = await axios.get('user', headers);
 
       console.log(response);
     }
