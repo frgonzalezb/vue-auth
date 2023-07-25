01f875e8ad865c58cdd8408edd8c39d381bc3959
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-25 14:49:10 -0400

    Routers

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import { HomeItem } from './components/HomeItem.vue'
+import { LoginItem } from './components/LoginItem.vue'
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeItem,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginItem,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
