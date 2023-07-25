fdea9bbed79de8d429d6f66ed61b4304164251f8
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-25 17:54:56 -0400

    Update NavbarItem.vue

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -3,12 +3,22 @@
     <div class="container">
       <router-link to="/" class="navbar-brand">Home</router-link>
       <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
+        <!-- If no user has logged in 👎 -->
+        <ul class="navbar-nav ml-auto" v-if="!user">
           <li class="nav-item">
             <router-link to="/login" class="nav-link">Login</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/register" class="nav-link">Sign-up</router-link>
+          </li>
+        </ul>
+        <!-- If someone has logged in 👍 -->
+        <ul class="navbar-nav ml-auto" v-if="user">
+          <li class="nav-item">
+            <!-- Usa javaScript:void(0) si, al hacer clic en un enlace, 
+              no quieres que el navegador abra una nueva página o refresque 
+              la página actual (dependiendo de la URL especificada). -->
+            <a href="javascript:void(0)" class="nav-link">Logout</a>
           </li>
         </ul>
       </div>
