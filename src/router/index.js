2b30648ddcccd8fdd19f96b6fdaa5b1a7a833d3a
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-25 15:15:00 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeItem from '../components/HomeItem.vue'
 import LoginItem from '../components/LoginItem.vue'
+import RegisterItem from '../components/RegisterItem.vue'
 
 const routes = [
   {
@@ -13,7 +14,12 @@ const routes = [
     path: "/login",
     name: "Login",
     component: LoginItem,
-  }
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: RegisterItem,
+  },
 ];
 
 const router = createRouter({
