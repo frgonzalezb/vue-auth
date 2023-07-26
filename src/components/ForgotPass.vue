5fff556f0872bd088f455113331a4e3bbc8d00f2
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-26 12:30:54 -0400

    Create ForgotPass.vue

---
+++ src/components/ForgotPass.vue
@@ -0,0 +1,20 @@
+<template>
+  <form>
+    <h3>Forgot password?</h3>
+
+    <div class="form-group">
+      <label for="recovery-email"></label>
+      <input type="email" class="form-control" name="email-field" id="recovery-email" placeholder="Email">
+    </div>
+
+    <button class="btn btn-primary btn-block">
+        Submit
+    </button>
+  </form>
+</template> 
+
+
+
+<script>
+
+</script>
