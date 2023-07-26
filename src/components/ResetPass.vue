46f5cb1ce3356ab7e5c60d50c0a931440ba8bad3
Francisco <fr.gonzalezb@duocuc.cl>
2023-07-26 12:48:39 -0400

    Update ResetPass.vue

--- src/components/ResetPass.vue
+++ src/components/ResetPass.vue
@@ -4,7 +4,12 @@
 
     <div class="form-group">
       <label for="new-pw">Password</label>
-      <input type="email" class="form-control" v-model="email" name="email-field" id="recovery-email" placeholder="Email">
+      <input type="password" class="form-control" v-model="newPass" name="new-pw" id="new-pw" placeholder="Password">
+    </div>
+
+    <div class="form-group">
+      <label for="confirm-new-pw">Confirm password</label>
+      <input type="password" class="form-control" v-model="confirmNewPass" name="confirm-new-pw" id="confirm-new-pw" placeholder="Confirm password">
     </div>
 
     <button class="btn btn-primary btn-block">
